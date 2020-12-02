33f64d01f62a69c2de31c1f3db218cfad8a5dbb9
justinkwanchan <justinkwanchan@gmail.com>
2020-12-02 19:05:35 +0000

    Added stdin

--- play.js
+++ play.js
@@ -3,3 +3,28 @@
 const { connect } = require('./client.js');
 console.log('Connecting ...');
 connect();
+
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      console.log('Disconnecting from server. Bye!');
+      process.exit();
+    }
+  }
+  
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
+setupInput();

58ea88f16d3d360f919b9309b421d2691eab03c9
justinkwanchan <justinkwanchan@gmail.com>
2020-12-02 20:20:38 +0000

    Added communcating movement from client to server

--- input.js
+++ input.js
@@ -1,15 +1,33 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    console.log('Disconnecting from server. Bye!');
-    process.exit();
+  switch (key) {
+    case '\u0003':
+      console.log('Disconnecting from server. Bye!');
+      process.exit();
+      break;
+    case 'w':
+      connection.write('Move: up');
+      break;
+    case 'a':
+      connection.write('Move: left');
+      break;
+    case 's':
+      connection.write('Move: down');
+      break;
+    case 'd':
+      connection.write('Move: right');
+      break;
   }
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
